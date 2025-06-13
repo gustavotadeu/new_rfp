e14330becdce96ffded8826fd6c707755020d167
gustavotadeu <40673430+gustavotadeu@users.noreply.github.com>
2025-06-13 17:43:38 -0300

    Fix celery worker command

--- backend/app/celery_app.py
+++ backend/app/celery_app.py
@@ -1,10 +1,12 @@
 from celery import Celery
 import os
 
+default_redis = 'redis://redis:6379/0'
+
 celery_app = Celery(
     'rfpgen',
-    broker=os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0'),
-    backend=os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'),
+    broker=os.getenv('CELERY_BROKER_URL', default_redis),
+    backend=os.getenv('CELERY_RESULT_BACKEND', default_redis),
 )
 
 celery_app.conf.task_routes = {'backend.app.tasks.*': {'queue': 'default'}}
